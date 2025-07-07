0b3c60c161c9bfd0cbddc0461535b1c29f1496e8
idontknowyet-wait-until-ifind <shilashm3@gmail.com>
2025-07-07 08:52:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,5 +16,4 @@ provider "hcp" {
 resource "hcp_packer_bucket" "example" {
   project_id   = "your-hcp-project-id"     # Replace with actual project ID
   name         = "my-packer-bucket"
-  description  = "This bucket stores image metadata for Ubuntu builds"
 }
